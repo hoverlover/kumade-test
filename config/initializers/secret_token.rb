661751069101970319360a5430b881fe4cde0199
hoverlover <hoverlover@gmail.com>
2011-10-15 08:27:32 -0500

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KumadeTest::Application.config.secret_token = 'cb9ffd9103ecc2a97d4f7c8074ecb314fd346f089280e8510d73be1930ab0a4d5531b62ff72505b1e4f7746bbbb197ae4c56ff7c1c11bc34ec515171ead809a0'
